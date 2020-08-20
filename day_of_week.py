c3f88d56a4747a54ad4409cf9c3255706ad0e60a
Dan Carter <dlcarterjr@yahoo.com>
2020-08-20 10:14:54 -0500

    Updated day_of_week for exception handling

--- day_of_week.py
+++ day_of_week.py
@@ -1,5 +1,12 @@
-day = input("Day (0-6) ? ")
-day = int(day)
+#Takes user input of a number with exception handling of TypeError
+while True:
+    try:
+        day = int(input("Day (0-6) ? "))
+        break
+    except ValueError:
+        print("Please enter a number.")
+
+#Checks user's number against the day of the week and outputs corresponding day.
 if day == 0:
     print("Sunday")
 elif day == 1:
--- prt_triangle.py
+++ prt_triangle.py
@@ -1,17 +1,16 @@
-count = 1
 levels = 4
-stars = ""
-while count <= levels:
-    if count == 1:
+row_count = 1
+star_count = 0
+last_stars = levels + (levels - 1)
+while row_count <= levels:
+    
+    while star_count <= last_stars:#prints stars after spaces on row.
         space_count = 1
-        while space_count <= levels - count:
-            stars = stars + " "
+        row = ""
+        while space_count <= levels - 1:#prints spaces before stars for one row.
+            row = row + "_"
             space_count += 1
-        stars = stars + "*"
-        print(stars)
-        stars = ""
-
-  
-    stars = stars + "**"
-    print(stars)
-    count += 1
+        row = row + "*"
+        star_count += 2
+    print(row)
+    row_count += 1
